b6d68af429fa3e003a0feda0ded1f7025e8e2e13
AnaAleksova <ana.aleksova30@gmail.com>
2025-06-28 20:14:04 +0200

    removed files

--- RealEstate.Repository/obj/Debug/net8.0/RealEstate.Repository.AssemblyInfo.cs
+++ RealEstate.Repository/obj/Debug/net8.0/RealEstate.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d989f6cccd6cbcf99c2fccb5c2b08b305f40432")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+479704f458a49500182cfbd1acdbe265d698fddb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
